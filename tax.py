f863ee656926458ada804ac341a9f202618ad15d
Kevin Navero <knavero@gmail.com>
2017-08-07 00:34:34 -0700

    able to successfully fetch down tables
    and convert them to a list of tuples containing the cell data

--- tax.py
+++ tax.py
@@ -1,62 +1,121 @@
 import math
+import re
 import urllib.request
+import xml.dom.minidom
+import xml.dom
+import html.entities
+from xml.parsers.expat import ExpatError
 from html.parser import HTMLParser
+from xml.dom.minidom import Node
 
-class TaxParser(HTMLParser):
-   def __init__(self, start_func, end_func, data_func):
-      self._start = start_func   
-      self._end = end_func
-      self._data = data_func
-      self._path = []
-      super().__init__()
+class TableParser(HTMLParser):
+   xhtml = { v: "&{};".format(k) 
+      for k, v in html.entities.html5.items()
+      if k in ['quot', 'amp', 'apos', 'lt', 'gt']}
+
+   @staticmethod
+   def _to_entity(match_obj):
+      m = match_obj.group(0)
+      if TableParser.xhtml.get(m):
+         return "{}".format(TableParser.xhtml[m])
+      return m
+
+   def __init__(self, *args, **kwargs):
+      self._capture = False
+      self._data = ''
+      super().__init__(*args, **kwargs)   
 
    def handle_starttag(self, tag, attrs):
-      self._start(tag, attrs)
+      if (tag == 'table'):
+         self._capture = True
+
+      if self._capture:
+         self._data += "<{}".format(tag)
+         if attrs:
+            for a in attrs:
+               self._data += " {}=\"{}\"".format(*a)
+         self._data += ">"
 
    def handle_endtag(self, tag):
-      self._end(tag)
+      if (self._capture):
+         self._data += "</{}>".format(tag)
+
+      if (tag == 'table'):
+         self._capture = False
 
    def handle_data(self, data):
-      self._data(data)
+      if self._capture:
+         data = data.strip()
+         if data:
+            data = re.sub(r'\S', TableParser._to_entity, data)
+            self._data += "{}".format(data)
+
+   def feed(self, data):
+      super().feed(data)
+      self._data = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" + \
+         "<!DOCTYPE html>" + \
+         "<html><body>" + self._data + "</body></html>"
+      return self._data
+
+class TaxTable:
+   @staticmethod
+   def _get_text_from_cell(node):
+      s = ''
+      for n in node.childNodes:
+         if (n.nodeType == Node.TEXT_NODE):
+            s += n.nodeValue
+         else:
+            s += TaxTable._get_text_from_cell(n)
+      return s
+
+   def __init__(self, table_elem):
+      self._table = table_elem
+
+   @property
+   def columns(self):
+      return len(self._table.getElementsByTagName('th'))
+
+   @property
+   def data(self):
+      datalist = [] 
+      data = []
+      cols = self.columns
+      idx = 0
+
+      datalist.append(tuple([TaxTable._get_text_from_cell(n) 
+         for n in self._table.getElementsByTagName('th')]))
+
+      tds = self._table.\
+         getElementsByTagName('tbody')[0].getElementsByTagName('td')
+      for n in tds: 
+         data.append(TaxTable._get_text_from_cell(n)) 
+         idx += 1
+         if idx % cols == 0:
+            datalist.append(tuple(data))
+            data.clear()
+
+      return datalist
 
 class TaxRequest:
-   @staticmethod
-   def _handle_starttag(tag, attrs):
-      print("start tag and attrs: {} {}".format(tag, attrs))
-
-   @staticmethod
-   def _handle_endtag(tag):
-      print("end tag: {}".format(tag))
-
-   @staticmethod
-   def _handle_data(data):
-      print("data: {}".format(data))
-
    def __init__(self, yr):
       self._url = "https://taxfoundation.org/{}-tax-brackets/".format(yr)
       with urllib.request.urlopen(self._url) as data:
-         self._content = data.read().decode()
-      self._tax_parser = TaxParser(
-         TaxRequest._handle_starttag,
-         TaxRequest._handle_endtag,
-         TaxRequest._handle_data )
+         content = data.read().decode()
+      tax_tables = TableParser().feed(content)
+      document = xml.dom.minidom.parseString(tax_tables)
+      self._tables = document.getElementsByTagName('table')
 
    @property
-   def content(self):
-      return self._content
-
-   def querySelectorAll(self, selector):
-      self._tax_parser.feed(self.content)
+   def tables(self):
+      return [TaxTable(table) for table in self._tables]
 
 class Taxable:
    def __init__(self, incomes):
       self._income = income   
 
-if __name__ == '__main__':
+def main():
    req = TaxRequest(2017)
-   print(req.querySelectorAll('table'))
+   return req.tables
 
-
-
-
-
+if __name__ == '__main__':
+   main()
